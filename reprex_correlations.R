51c4c3131bbde99d511bc4ed11e20cbc2cef8ed3
Jason Craggs <jcraggs@Jasons-MacBook-Pro.local>
2019-04-17 21:02:46 -0500

    seeking help on correlations

---
+++ reprex_correlations.R
@@ -0,0 +1,23 @@
+#	Reprex correlations
+#	modified: "17/04/2019"
+
+library(readr)	# Some additional options for reading csv data
+library(purrr)
+library(ggplot2)
+# library(modelr)
+library(Hmisc)
+# library(doBy)
+# require(dplyr)
+library(ez)
+# library(stringr)
+library(corrplot)
+options(na.action = na.exclude)
+library(PerformanceAnalytics)
+library(tidyverse)
+library(here)
+here() # THIS SETS THE WORKING DIRECTORY TO THE LOCATION OF THIS FILE
+dr_here(show_reason = FALSE)
+
+
+
+
