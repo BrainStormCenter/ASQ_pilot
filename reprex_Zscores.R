c8bf4dc4d59a2a4f67664c5387ab438ac433629e
Jason <chejux@gmail.com>
2019-05-03 12:47:19 -0500

    updates for calculating z-scores by row

--- reprex_Zscores.R
+++ reprex_Zscores.R
@@ -1,23 +1,44 @@
 # title: "reprex_Zscores"
 # author: "Jason"
 # date: "2019_04_30"
+#modified: "03/05/2019"
 
 library(tidyverse)
 library(reprex)
-library(purrr)
-library(here)
+library(dplyr)
+# library(purrr)
+# library(here)
 
-## IMPORT DATA FOR THE PILOT ANALYSES
+## Community answer
+
 dat <- read.csv(url('https://raw.githubusercontent.com/BrainStormCenter/ASQ_pilot/master/ASQ_pain_pilot_2019_04_19_v2.csv'), header = TRUE)
-dat <- as_tibble(dat)
-dat <- filter(dat, Groups != "other")
+#		This creates z-scores for a variable/vector and works
+# z.dat1 <- dat %>%
+# 	as_tibble() %>%
+# 	filter(Groups != "other") %>%
+# 	select(ID, sex, asq_1a, asq_2a, asq_3a, asq_4a, asq_5a, asq_6a,
+# 		   asq_7a, asq_8a, asq_9a) %>%
+# 	mutate_at(vars(starts_with("asq")), list(z1 = ~as.vector(scale(.))))
 
-#		CREATE A SMALLER DATA SET
-dat.asq <- select(dat, ID, sex, asq_1a, asq_2a, asq_3a, asq_4a)
-#		CLACULATE Z-SCORES
-dat.asq2 <- as.data.frame(scale(dat.asq[,c(grep("a$", colnames(dat.asq)))],
-								center = TRUE, scale = TRUE))
-#		ADD PREFIX TO CALCULATED SCORES
-names(dat.asq2) <- paste0("z", names(dat.asq2))
-#		ADD CALCULATED Z-SCORES TO INITIAL DATA SET
-dat.asq3 <-  bind_cols(dat.asq, dat.asq2)
+#		Trying to calculate z-scores rowwise
+#		Colums are made but valvues are NaN
+z.dat2 <- dat %>%
+	as_tibble() %>%
+	filter(Groups != "other") %>%
+	select(ID, sex, asq_1a, asq_2a, asq_3a, asq_4a, asq_5a, asq_6a,
+		   asq_7a, asq_8a, asq_9a) %>%
+	rowwise() %>%
+	mutate_at(vars(starts_with("asq")), list(zz = ~as.vector(scale(.))))
+
+
+z.dat2 <- dat %>%
+	as_tibble() %>%
+	filter(Groups != "other") %>%
+	select(ID, sex, asq_1a, asq_2a, asq_3a, asq_4a, asq_5a, asq_6a,
+		   asq_7a, asq_8a, asq_9a) %>%
+	gather(asq, value, -ID, -sex) %>%
+	group_by(ID) %>%
+	mutate(z = as.vector(scale(value))) %>%
+	pivot_wider(names_from = asq, values_from = c(value, z))
+
+z.dat2
