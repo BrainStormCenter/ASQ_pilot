6df2ba1aa6d1f639a47ae5b6aca333c6868c09c9
Jason <chejux@gmail.com>
2019-05-04 19:45:50 -0500

    updated analyses and plots

--- reprex_Zscores.R
+++ reprex_Zscores.R
@@ -21,24 +21,18 @@ dat <- read.csv(url('https://raw.githubusercontent.com/BrainStormCenter/ASQ_pilo
 # 	mutate_at(vars(starts_with("asq")), list(z1 = ~as.vector(scale(.))))
 
 #		Trying to calculate z-scores rowwise
-#		Colums are made but valvues are NaN
+#		Community answer
 z.dat2 <- dat %>%
 	as_tibble() %>%
 	filter(Groups != "other") %>%
-	select(ID, sex, asq_1a, asq_2a, asq_3a, asq_4a, asq_5a, asq_6a,
-		   asq_7a, asq_8a, asq_9a) %>%
-	rowwise() %>%
-	mutate_at(vars(starts_with("asq")), list(zz = ~as.vector(scale(.))))
-
-
-z.dat2 <- dat %>%
-	as_tibble() %>%
-	filter(Groups != "other") %>%
-	select(ID, sex, asq_1a, asq_2a, asq_3a, asq_4a, asq_5a, asq_6a,
-		   asq_7a, asq_8a, asq_9a) %>%
-	gather(asq, value, -ID, -sex) %>%
+	select(ID, sex, ends_with("a")) %>%
+	gather(asq, value, -ID, -sex, na.rm = FALSE) %>%
 	group_by(ID) %>%
 	mutate(z = as.vector(scale(value))) %>%
 	pivot_wider(names_from = asq, values_from = c(value, z))
 
 z.dat2
+
+z.dat3 <- select(z.dat2, ID, starts_with("z"))
+# merge two data frames by ID
+dat <- merge(dat, z.dat3, by = "ID")
