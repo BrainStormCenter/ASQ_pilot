a7021c4e55571dc08911eecf64879dd8019ed0fe
Jason <chejux@gmail.com>
2019-05-15 00:20:11 -0500

    package issues

---
+++ reprex_tidyverse.R
@@ -0,0 +1,13 @@
+# title: "reprex_tidyverse"
+# author: "Jason"
+# date: 14/05/2019
+
+library(tidyverse)
+library(ez)
+library(fuzzyjoin)
+library(ggplot2)
+library(plotly)
+library(factoextra)
+library(dplyr)
+
+library(purrr)
